454facb4f77fe9e506e6154142d6846a1796e9e4
youssef esmail <youssef.esmailelfeky@gmail.com>
2025-08-21 12:40:01 +0300

    Initial Commit

---
+++ ThmanyahTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThmanyahTask
+//
+//  Created by Mader's Macbook Pro on 21/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThmanyahTask/ThmanyahTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThmanyahTaskApp.swift
+//  ThmanyahTask
+//
+//  Created by Mader's Macbook Pro on 21/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ThmanyahTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
